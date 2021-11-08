6365cd8262a3341a7e458bf4088211ccd0a0e667
Wozzify <63321091+Wozzify@users.noreply.github.com>
2021-11-08 01:16:30 +0000

    Update Snapchatdownloader.js
    Updated it to work on Chrome/Firefox.
    
    Albeit Chrome multiple downloads even with fully disabled automatic download protection doesn't seem to work, so best to download Firefox.

--- Snapchatdownloader.js
+++ Snapchatdownloader.js
@@ -6,12 +6,14 @@ var go = 0;
 function download(){
     var links = document.getElementsByTagName("table")[0].getElementsByTagName("a");
     //Finds all of the "download" button entities and putting them under the variable 'links'
-    eval(links[go].href);
-    //Is evaluating the download button with the variable 0 so you start off with the first one
+    links.value = go;
+    //Mixes links and go into the [array]
+    links[go].click();
+    //Clicks each "download" starting from 'go = 0'
+    console.log(go);
+    //Keeps progress of how many downloads you have initated (Can run behind if you have it on a high setting) seems to be glitchy on firefox.
     go++;
     //Makes the variable 0 go up in 1 increment everytime it gets to this part
-    console.log(go);
-    //Keeps progress of how many downloads you have initated (Can run behind if you have it on a high setting)
     setTimeout(download, 5000);
     //If you don't have a timeout it will just try and download all of them at once. This is the optimum speed but obviously
     //feel free to increase or decrease.
